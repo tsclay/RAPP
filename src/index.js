61d7a2cd8b4640fd3e7c2025c0294fd2fd92fac1
Tim <tsclay9@gmail.com>
2020-08-08 16:02:55 -0400

    add env and conditionals for API routes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,24 @@ import './index.css';
 import HelloMessage from './components/HelloMessage';
 import PersonForm from './components/PersonForm'
 
+// Change to not 'dev' when ready for production
+const ENV = "prod";
+
+// API route that will change based on env setting
+// Add to these as necessary
+let PEOPLE = '';
+
+if (ENV === 'dev') {
+  PEOPLE = "http://localhost:8888/people"
+} else {
+  PEOPLE = "/people";
+}
+
+// If adding extra API routes, make sure to pass them down as props
 ReactDOM.render(
   <React.StrictMode>
     <HelloMessage />
-    <PersonForm/>
+    <PersonForm API_URL={PEOPLE}/>
   </React.StrictMode>,
   document.getElementById('root')
 );

8e59da19766b673af104249d4dbc4bfac5e1ef43
Tim Clay <tsclay9@gmail.com>
2020-08-09 01:28:56 -0400

    adding React build to mix

---
+++ build/precache-manifest.4cd37c5ac706d823c7b0ac991fd24f8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d92e07165328a10f4b51f9bfcac0ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08bdbbcd09061a611394",
+    "url": "/static/css/main.d71ce233.chunk.css"
+  },
+  {
+    "revision": "495e93aa5b343134c751",
+    "url": "/static/js/2.b2783baf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b2783baf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08bdbbcd09061a611394",
+    "url": "/static/js/main.4d0ead11.chunk.js"
+  },
+  {
+    "revision": "e0b44b1fb979c291c355",
+    "url": "/static/js/runtime-main.329e42b0.js"
+  }
+]);

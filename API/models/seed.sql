6591d1a17c716405035a86b2574dfd60c58b0baf
Tim <tsclay9@gmail.com>
2020-08-10 00:45:55 -0400

    created user and password for dev env

--- API/models/seed.sql
+++ API/models/seed.sql
@@ -1,11 +1,16 @@
 DROP DATABASE IF EXISTS contacts;
+DROP USER IF EXISTS rapp;
+
+CREATE USER rapp WITH PASSWORD 'reactPHP';
 
 CREATE DATABASE contacts;
+ALTER DATABASE contacts OWNER TO rapp;
 
 \c contacts;
 
 CREATE TABLE people
 (id SERIAL, name VARCHAR(255), age INT);
+ALTER TABLE people OWNER TO rapp;
 
 INSERT INTO people (name, age) VALUES
 ('Bobby', 45),
